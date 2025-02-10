c057a3259df57a705957052f37b1ae09495936af
Ahmed Ajanovic <ajanovicahmed11@hotmail.com>
2025-02-10 15:21:23 +0100

    fix little mistake in update

--- app/_components/RegisterForm.js
+++ app/_components/RegisterForm.js
@@ -27,9 +27,9 @@ function RegisterForm({ title, buttonText, user, token }) {
 
   let realUser = {
     ...user,
-    name: name === user.name ? user.name : name,
-    surname: surname === user.surname ? user.surname : surname,
-    email: email === user.email ? user.email : email,
+    name: name === user?.name ? user.name : name,
+    surname: surname === user?.surname ? user.surname : surname,
+    email: email === user?.email ? user.email : email,
   };
 
   return (
